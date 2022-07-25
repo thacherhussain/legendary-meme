05d7e0625b5a326bbd772e6db12f3213a214b6dc
Thacher Hussain <thacher.hussain@gmail.com>
2022-07-25 13:16:53 -0400

    react-native-screens config for Android

--- android/app/src/main/java/com/trackee/MainActivity.java
+++ android/app/src/main/java/com/trackee/MainActivity.java
@@ -1,3 +1,4 @@
+import android.os.Bundle;
 package com.trackee;
 
 import com.facebook.react.ReactActivity;
@@ -43,6 +44,11 @@ public class MainActivity extends ReactActivity {
       // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
       // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
       return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(null);
     }
   }
 }

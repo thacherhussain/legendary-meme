a6ba4e1ccc96969cd3fce7ecc379da14b655e980
Thacher Hussain <thacher.hussain@gmail.com>
2022-10-19 15:19:21 -0400

    storybook global backgrounds

--- storybook/addons/paddingAddon.tsx
+++ storybook/addons/paddingAddon.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { View } from 'react-native'
 import { NativeBaseProvider } from 'native-base'
+
 import { theme } from '../../src/utils/theme'
 
 export const paddingAddon = (storyFn) => (
   <NativeBaseProvider theme={theme}>
-    <View style={{ flex: 1, width: '100%', padding: 40 }}>{storyFn()}</View>
+    <View
+      style={{
+        flex: 1,
+        width: '100%',
+        padding: 40,
+      }}
+    >
+      {storyFn()}
+    </View>
   </NativeBaseProvider>
 )

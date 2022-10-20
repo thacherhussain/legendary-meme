31308cbe28f0921050e84f49b67fec8db3e8bfe6
Thacher Hussain <thacher.hussain@gmail.com>
2022-10-19 21:35:31 -0400

    removed leave button story

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,13 +4,11 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  require('./stories/components/Leave.stories')
   require('./stories/components/Section.stories')
   require('./stories/components/Text.stories')
 }
 
 const stories = [
-  './stories/components/Leave.stories',
   './stories/components/Section.stories',
   './stories/components/Text.stories',
 ]
